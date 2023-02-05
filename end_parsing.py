83307e2d50de35fec08503a27f2ba06ef2cbd7bb
Viktor <viktordee41@icloud.com>
2023-02-05 19:16:55 +0300

    first commit

---
+++ end_parsing.py
@@ -0,0 +1,95 @@
+import openpyxl
+import vk_api
+import csv
+import re
+
+# Имя;Фамилмя;id_user;Пол;Город;Год Рождения;id_group
+# -------------------------------------------------
+book_tess = openpyxl.open("12.xlsx") #Выбор файла с данными групп
+sheets = book_tess.sheetnames
+data_excel = book_tess[sheets[0]] #Тут можно выбрать лист
+stolbik = 1 #выбор столбца
+stroka = 0 # выбор с какой строки начнет
+
+
+# WORK VK
+session = vk_api.VkApi(token="vk1.a.qS-6u3AvUyMKH_q5038CbWuRHKf_mxa2Cpz9tvWq2OgECseZBq0XW92hL27VkwHYdPf2vAFZAHl5PsycOdfY8TLVuHB6XwSEVThx8oUUQE8woksiiU0qk7cQmQxdGk2XB483-bnv2h8TbDlHuBzDBSuI4a5rKEVjeHCuJWWQ_hkERk1cKUZtop6wsljoP23kqFs0lINfRz1BekLO_U87eQ")
+vk = session.get_api()
+
+def pars_memb(group_id):
+    offset = 0
+    count = 1000
+    has_more_mambers = True
+    resoult = []
+    while has_more_mambers == True:
+        response = session.method("groups.getMembers", {
+            "group_id" : group_id,
+            "fields" : "sex , city , bdate , domain",
+            "offset" : offset,
+            "count" : count
+            })
+        print(offset,len(response["items"]))
+        has_more_mambers = bool(len(response["items"]))
+        offset += count
+        resoult = resoult + response["items"]
+    
+    return resoult
+
+def get_user_city(user):
+    try:
+        return user["city"]["title"]
+    except:
+        return None
+
+def get_user_by(user):
+    try:
+        date = user["bdate"].split(".")
+        if len(date) == 3:
+            return date[-1]
+    except:
+        return None
+
+# Сохранение файла
+def save_group_members(users):
+    with open('moskow.csv','a',encoding='utf8', newline='') as File:  #Задать название файла
+        writers = csv.writer(File,delimiter=';')
+        for user in users:
+            writers.writerow([
+                user.get("first_name",""),
+                user.get("last_name",""),
+                user.get("domain",""),
+                user.get("sex",""),
+                get_user_city(user),
+                get_user_by(user),
+                goko
+            ])
+
+
+for urlrs in range(stolbik,data_excel.max_row):
+    goko = data_excel[urlrs][stroka].value
+    znachenieForUrl = data_excel[stolbik][stroka].value
+    print(znachenieForUrl)
+    if "m/" in znachenieForUrl:
+        goko = znachenieForUrl.split("m/")[1]
+        print(goko)
+
+    if re.findall(r"club\d+", goko):
+        nums = re.findall(r'\d+', goko)
+        nums = [int(i) for i in nums]
+        non_club = " ".join(map(str,nums))
+        print(non_club)
+        members = pars_memb(non_club)
+        save_group_members(members)
+    elif re.findall(r"public\d+", goko):
+        nums = re.findall(r'\d+', goko)
+        nums = [int(i) for i in nums]
+        non_club = " ".join(map(str,nums))
+        print(non_club)
+        members = pars_memb(non_club)
+        save_group_members(members)
+    else:
+        try:
+            members = pars_memb(goko)
+            save_group_members(members)
+        except:
+            None
--- plot_date.py
+++ plot_date.py
@@ -1,29 +1,16 @@
-from tokenize import Name
-from traceback import print_tb
-from turtle import title
-from joblib import parallel_backend
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import Counter
 import seaborn as sns
 from sklearn.cluster import KMeans
-# from numba import njit, prange,jit
-import time
 
 df = pd.read_csv('group_3.csv', sep=";", engine="c")
-# df = np.genfromtxt('group_1.csv',delimiter=";")
 df = df.drop_duplicates(keep='first')
-
 
 def plot_age(df):
     df = df[df['Год Рождения'].notna()]
-    # print(df)
-    # df['Год Рождения'] = df['Год Рождения'].fillna(0)
-    # print(df['Год Рождения'])
-    # df["Год Рождения"] = df["Год Рождения"].astype(int)
     df = df[df['Год Рождения'] > 1970]
-    # print(df['Год Рождения'])
     df['Год Рождения'] = 2022 - df['Год Рождения']
     mean_df = df['Год Рождения'].mean()
     print(mean_df)
@@ -31,42 +18,33 @@ def plot_age(df):
     plt.hist(df["Год Рождения"], bins=50)
     plt.xticks(np.arange(min(df["Год Рождения"]), max(
         df["Год Рождения"]+1), step=1), rotation=45)
-    # plt.xticks(np.arange(1970,2008,step=2),rotation=45)
     plt.grid()
     plt.savefig('график_Год_Рождения.pdf')
     plt.show()
 
-
 def plot_hist_city(stolb):
     letter_counts = Counter(stolb)
     s = stolb.name
-    # print(letter_counts)
     dff = pd.DataFrame.from_dict(letter_counts, orient='index')
     dff = dff[dff > 5000]
     dff = dff.dropna()
     dff = dff.drop(np.NaN, axis=0)
-    # dff = dff.notna()
-    # print(dff)
     dff.plot(kind='bar', stacked=True)
-    # plt.xticks(rotation=45)
     plt.suptitle(str(s))
     print(dff)
     plt.show()
     plt.savefig('график_' + s + '.pdf')
-
 
 def plot_hist_sex(stolb):
     letter_counts = Counter(stolb)
     s = stolb.name
     dff = pd.DataFrame.from_dict(letter_counts, orient='index')
     dff = dff.dropna()
-    # dff = dff.notna()
     dff.plot(kind='bar', stacked=True)
     plt.suptitle(str(s))
     plt.legend(["2 - Мужчины \n 1 - Женщины"])
     plt.show()
     plt.savefig('график_' + s + '.pdf')
-
 
 def wm_pie_proc(df):
     Womens = len(df[df["Пол"].to_numpy() == 1])
@@ -78,18 +56,13 @@ def wm_pie_proc(df):
     plt.pie(Mens_Womens, labels=('Women', 'Men'), colors=colors,
             explode=explode, shadow=True, autopct='%1.1f%%')
     plt.show()
-    # print(Mens_Womens)
-
 
 def kdensity_date(df):
     df = df[df['Год Рождения'].notna()]
     df = df[df['Год Рождения'] > 1970]
     df = df[df['Год Рождения'] < 2014]
-    # plt.xticks(min(df["Год Рождения"]),max(df["Год Рождения"]+1),step=1,rotation=45)
     df["Год Рождения"].plot.kde()
-    # print(type(df['Год Рождения']))
     plt.show()
-
 
 def Raspredelenie_po_city(stolb, top):
     letter_counts = Counter(stolb)
@@ -102,7 +75,6 @@ def Raspredelenie_po_city(stolb, top):
     dff = dff[0:top+1]
     dff.plot(kind='bar', stacked=True)
     plt.suptitle(str(s))
-    # plt.xticks(rotation=45)
     print(dff)
     plt.show()
     plt.savefig('график_' + s + '.pdf')
@@ -118,33 +90,9 @@ def klaster(df):
     f = df['Город'].value_counts()[df['Город'].value_counts() > 10000]
     plt.scatter(df['Год Рождения'], df['Город'].value_counts,
                 c=df["cluster"], cmap="rainbow")
-    # plt.xlim(180, -180)
-    # plt.ylim(0.5, 2.5)
-    # plt.show()
-    # print(df['Город'].value_counts)
-
-# print(df['Город'].value_counts()[df['Город'].value_counts() > 10000])
-# df_city_to_int = dict(zip(list(df['Город']),[list(df['Город']).count(i) for i in list(df['Город'])]))
-# print(df_city_to_int.values())
-# df_city =
-# print(df.iloc[:,[2,3]])
-# print(df['Город'].value_counts())
-# print(df.head())
-
 
 def city_to_age(df):
-    # (df['Город'].dropna().map(str) + '-' + (2022 - df['Год Рождения'].dropna()).map(str)).value_counts()[:10].plot(kind='bar')
     pot = (df['Город'].dropna().map(str) + '-' + (2022 -
            df['Год Рождения'].dropna()).map(str)).value_counts()[:10]
     pot.plot.barh()
     plt.show()
-
-
-# plot_hist_sex(df["Пол"])
-# plot_hist_city(df["Город"])
-# plot_age(df)
-# wm_pie_proc(df)
-# kdensity_date(df)
-# Raspredelenie_po_city(df["Город"],20)
-# klaster(df)
-# city_to_age(df)
